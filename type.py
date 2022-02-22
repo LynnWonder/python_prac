437d2e6f63f4a8c867035cbb2c408621dc27aa6c
liulin.wonder <liulin.wonder@bytedance.com>
2022-02-22 11:09:44 +0800

    test __file__ & pathlib

--- type.py
+++ type.py
@@ -1,5 +1,6 @@
 # tip 这一节介绍判断对象类型的几种方式
 import types
+from pathlib import Path
 
 # tip type 基本上可以判断大部分对象的类型，除了 class 的类型不太方便一般都是用 isinstance
 #   因此一般优先使用 isinstance
@@ -26,4 +27,11 @@ print("是否有属性 x==>", hasattr(obj, 'x'))
 print("获取属性 x==>", getattr(obj, 'x'))
 print("设置属性 x==>", setattr(obj, 'y', 10))
 print("获取属性 y==>", getattr(obj, 'y'))
-print("获取属性 z==>", getattr(obj, 'z'))
+# 会报错
+# print("获取属性 z==>", getattr(obj, 'z'))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR 是当前项目的绝对地址
+print('here=====>', BASE_DIR)
+# __file__ 是当前「引用」其他文件的文件的绝对地址(即当前文件的绝对地址)
+print('here=====>', __file__)
