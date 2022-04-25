cd143143e26163fdbf99478151dce0e18cdbf4c7
liulin.wonder <liulin.wonder@bytedance.com>
2022-04-25 20:51:03 +0800

    opt: add some comment in thread and async

--- asyncio_learn.py
+++ asyncio_learn.py
@@ -2,9 +2,16 @@ import threading
 import asyncio
 
 
+# TIP 异步编程的不同实现方式：
+#  我们为什么需要多线程或者多进程：
+#  在 IO 操作中，当前线程被挂起，其他需要 CPU 执行的代码就没办法执行了，因此我们使用多线程或者多进程来并发执行代码
+# TIP 使用异步 IO 的目的
+#  因为系统不能无限的增加线程，切换线程的开销也很大。所以使用多线程或者多进程只是解决 CPU 高速执行能力和 IO 设备的龟速严重不匹配问题的方法之一
+#  另一种方法就是使用异步 IO
+
 async def hello():
     print('Hello world! (%s)' % threading.currentThread())
-    await asyncio.sleep(1)
+    await asyncio.sleep(10)
     print('Hello again! (%s)' % threading.currentThread())
 
 
--- thread_learning.py
+++ thread_learning.py
@@ -1,9 +1,9 @@
-# TIP 多任务可以由多进程完成，也可以由一个进程内的多线程完成
+# TIP 使用多线程来实现异步编程（多线程是异步编程的一种实现方式）
+#  多任务可以由多进程完成，也可以由一个进程内的多线程完成
 #  一个进程至少有一个线程，线程是操作系统直接支持的执行单元
 #  多进程中同一个变量各自拷贝一份存在每个进程中互不影响
 #  多线程中所有变量都由所有线程共享
-#  同时需要注意：python 不能利用多线程实现多核任务，但是可以通过多进程实现多核任务。
-#          多个 python 进程有各自独立的 GIL 锁，互不影响
+#  同时需要注意：python 不能利用多线程实现多核任务，但是可以通过多进程实现多核任务。多个 python 进程有各自独立的 GIL 锁，互不影响
 
 import time, threading
 
@@ -18,11 +18,13 @@ def loop():
         time.sleep(1)
     print('thread %s ended.' % threading.current_thread().name)
 
-# 任何进程默认启动一个线程，即主线程
-print('thread %s is running...' % threading.current_thread().name)
-t = threading.Thread(target=loop, name='LoopThread')
-t.start()
-# TIP t.join() 会阻塞线程 t 直到 t 终结，有点类似于 await 的含义
-t.join()
-#  TIP 没有 t.join() 的时候会一开始就执行下面这段代码
-print('thread %s finished...' % threading.current_thread().name)
+
+if __name__ == '__main__':
+    # 任何进程默认启动一个线程，即主线程
+    print('thread %s is running...' % threading.current_thread().name)
+    t = threading.Thread(target=loop, name='LoopThread')
+    t.start()
+    # TIP t.join() 会阻塞线程 t 直到 t 终结，有点类似于 await 的含义
+    t.join()
+    #  TIP 没有 t.join() 的时候会一开始就执行下面这段代码
+    print('thread %s finished...' % threading.current_thread().name)
